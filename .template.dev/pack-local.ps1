8148b6ae33bec5d1bf74d026fa68ca0a0fa41d46
markus.gnigler <markus.gnigler@gmx.at>
2022-03-26 03:22:02 +0100

    Update template.dev folder

---
+++ .template.dev/pack-local.ps1
@@ -0,0 +1,16 @@
+cd ../..
+
+$Version = "1.0.0"
+
+echo "Pack version: $Version"
+
+dotnet pack ./nuget.csproj `
+   --configuration Release `
+   --output artifacts `
+   /p:Version=$Version
+
+rm -R bin, obj
+
+cd .template.config
+
+pause
