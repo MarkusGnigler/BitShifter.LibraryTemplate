382ac530a71b3f5764b637d7a20d6787399bc4f5
markus.gnigler <markus.gnigler@gmx.at>
2022-03-26 03:24:08 +0100

    Update publish script

--- Docs/Scripts/pack-and-publish.ps1
+++ Docs/Scripts/pack-and-publish.ps1
@@ -5,6 +5,8 @@ $NugetApiKey = ""
 
 docker build --tag publish --build-arg Version=$LibraryVersion --file Dockerfile.Pack .
 
-docker run --rm --publish 443:443 publish --api-key $NugetApiKey --source "https://api.nuget.org/v3/index.json"
+docker run --rm --volume ${PWD}/artifacts:/sln/artifacts publish
+
+# docker run --rm --volume ${PWD}/artifacts:/sln/artifacts publish --api-key $NugetApiKey --source "https://api.nuget.org/v3/index.json"
 
 exit 0
