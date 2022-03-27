f8528ce8cea22255f37c9a97f3c61b161f8a8e28
markus.gnigler <markus.gnigler@gmx.at>
2022-03-27 13:28:31 +0200

    Remove unused scripts

--- .template.dev/pack-and-publish.ps1
+++
@@ -1,7 +0,0 @@
-cd ..
-
-docker build --tag publish --build-arg Version=1.0.1 .
-
-docker run --rm publish --api-key ${ApiKey} --source "https://api.nuget.org/v3/index.json"
-
-exit 0
--- .template.dev/test-all-parameters.ps1
+++
@@ -1,9 +0,0 @@
-
-dotnet new library -o Company.Net5 --framework net5.0
-dotnet new library -o Company.Net6 --framework net6.0
-dotnet new library -o Company.Test --tests false
-dotnet new library -o Company.Github --pipeline github
-dotnet new library -o Company.Gitlab --pipeline gitlab
-# dotnet new library -o Company.Docker --docker false
-
-exit 0
